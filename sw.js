4c5c28c1fbf9ce46d59542e58cbce3724752b1a5
DrEhabTurki <89798994+DrEhabTurki@users.noreply.github.com>
2026-05-10 00:46:05 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-self.addEventListener('fetch', function(event) {
-  // هذا الكود يسمح للتطبيق بالعمل كـ PWA
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // بيسمح للتطبيق يشتغل أوفلاين
 });
